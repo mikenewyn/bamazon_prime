f27c553a359b10b4466f4f1cf4d3dc624f54d654
Michael Nguyen <thinknownotforever@gmail.com>
2018-12-20 21:28:51 -0800

    table set up and connected to console log

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,6 +17,18 @@ var connection = mysql.createConnection({
   
   connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    //createProduct();
-  });
+    console.log("Successfully connected as id " + connection.threadId + "\n");
+    createTable();
+  })
+
+  var createTable = function(){
+    connection.query("SELECT * FROM products", function(err, res){
+      for(var i = 0; i<res.length; i++){
+        console.log(res[i].item_id 
+          + " --- " + res[i].product_name 
+          + " --- " + res[i].department_name 
+          + " --- " + res[i].price 
+          + " --- " + res[i].stock_quantity + "\n");
+      }
+    })
+  }
